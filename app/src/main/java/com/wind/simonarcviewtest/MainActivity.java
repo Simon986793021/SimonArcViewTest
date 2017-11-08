e7742d816fb13b014519fea97c028cea9b61aaa5
simon986793021 <986793021@qq.com>
2017-11-08 17:37:39 +0800

    添加viewpager弧形高度属性

--- app/src/main/java/com/wind/simonarcviewtest/MainActivity.java
+++ app/src/main/java/com/wind/simonarcviewtest/MainActivity.java
@@ -8,14 +8,10 @@ import com.wind.arcview.HomeBanner;
 import com.wind.arcview.SimonArcView;
 
 public class MainActivity extends AppCompatActivity {
-    private SimonArcView arcView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HomeBanner homeBanner= (HomeBanner) findViewById(R.id.hb_banner);
-        homeBanner.setImagesRes(new int[]{R.drawable.banner5,R.drawable.banner5,R.drawable.banner5,R.drawable.banner5});
-        ArcImageView arcImageView=new ArcImageView(this);
     }
 }
